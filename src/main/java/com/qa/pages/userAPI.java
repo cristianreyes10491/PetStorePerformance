a0ee446cec8227b7e4bcd085b1bb2401df9cdea0
Cristian Reyes <cristian.reyes1049@gmail.com>
2024-06-10 12:05:41 -0500

    10jun243

--- src/main/java/com/qa/pages/userAPI.java
+++ src/main/java/com/qa/pages/userAPI.java
@@ -1,21 +1,25 @@
 package com.qa.pages;
 
-/*import io.restassured.response.Response;
-import static io.restassured.RestAssured.given;
-public class userAPI {
-    private static final String USERS_ENDPOINT = "/users";
+//import io.restassured.response.Response;
+//import static io.restassured.RestAssured.given;
 
-    public Response getUserById(int userId) {
+
+public class userAPI {
+/*    private static final String USERS_ENDPOINT = "/users";
+
+    public Response petId(int petId) {
         return given()
-                .pathParam("id", userId)
+                .pathParam("id", petId)
                 .when()
                 .get(USERS_ENDPOINT + "/{id}");
 }
-    public Response createUser(String name, String email) {
+   /* public Response createUser(String name, String email) {
         return given()
                 .contentType("application/json")
                 .body("{ \"name\": \"" + name + "\", \"email\": \"" + email + "\" }")
                 .when()
                 .post(USERS_ENDPOINT);
-    }
-}*/
+
+    } */
+
+}
